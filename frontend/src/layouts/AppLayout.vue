bab2c38ea6cd9255be18a5023aaff49512d72af8
mateusvrs <mateusvrs.dev@gmail.com>
2025-05-18 21:37:43 -0300

    feat: set footer as default to app layout

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Footer from '@/components/base/Footer/Footer.vue';
+import FooterIconLink from '@/components/base/Footer/FooterIconLink.vue';
+</script>
+
+<template>
+    <div
+      class='grid grid-rows-10 h-screen'
+    >
+    <slot />
+    <Footer>
+      <FooterIconLink
+        target='about'
+        normal='fluent:home-12-regular'
+        active='fluent:home-12-filled'
+      />
+      <FooterIconLink
+        target='shopping'
+        normal='solar:bag-4-outline'
+        active='solar:bag-4-bold'
+      />
+      <FooterIconLink
+        target='news'
+        normal='majesticons:note-text-line'
+        active='majesticons:note-text'
+      />
+    </Footer>
+  </div>
+</template>
