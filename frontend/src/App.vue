3b844d5be742861e0eb7b2f480f207932748c6e4
mateusvrs <mateusvrs.dev@gmail.com>
2025-05-18 21:36:35 -0300

    feat: routing pages with custom layouts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <RouterView />
+  <component :is='layout'>
+    <router-view />
+  </component>
 </template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+
+import { useRoute } from 'vue-router';
+
+import EmptyLayout from './layouts/EmptyLayout.vue';
+import AppLayout from './layouts/AppLayout.vue';
+
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+
+    const layout = computed(() => {
+      const layoutMap = {
+        app: AppLayout,
+        empty: EmptyLayout
+      } as const;
+
+      const layoutKey = (route.meta.layout as keyof typeof layoutMap) || 'app';
+      return layoutMap[layoutKey];
+    });
+
+    return {
+      layout
+    };
+  }
+});
+</script>
