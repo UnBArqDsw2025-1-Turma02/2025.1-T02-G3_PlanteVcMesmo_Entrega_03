400bb653f2f5aa97665eded5bd5a2adff950f3ce
mateusvrs <mateusvrs.dev@gmail.com>
2025-05-18 21:55:37 -0300

    refactor: change page to portuguese

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -2,14 +2,14 @@
   <main
     class='flex flex-col gap-1 items-center justify-center h-screen bg-secondary-green'
   >
-    <h1>404 - Page Not Found</h1>
-    <p>The page you're looking for doesn't exist.</p>
+    <h1>404 - Página não encontrada</h1>
+    <p>A página que você está procurando não existe.</p>
     <p>
       <router-link
         class='text-indigo-100 underline'
         to='/'
       >
-        Click here to go back to the homepage
+        Clique aqui para voltar à página inicial
       </router-link>
     </p>
   </main>
