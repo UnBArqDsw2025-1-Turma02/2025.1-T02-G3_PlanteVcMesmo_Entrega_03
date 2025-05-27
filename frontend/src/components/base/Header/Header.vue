9eccbcc475aedf0fd10b75ae5efadf9e1e67059f
mateusvrs <mateusvrs.dev@gmail.com>
2025-05-27 08:54:28 -0300

    feat: header + main components for page struct

---
+++ frontend/src/components/base/Header/Header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+import { useAuth } from '@/hooks/use-auth';
+
+const { user } = useAuth();
+
+const route = useRoute();
+</script>
+
+<template>
+  <header
+    class='shadow-lg row-span-3 bg-secondary-green'
+  >
+    <router-link
+      v-if="user && route.name !== 'profile'"
+      to="/profile"
+    >
+      <img
+        class="rounded-lg fixed right-3 top-3 h-[50px] w-[50px]"
+        src="https://picsum.photos/110"
+      />
+    </router-link>
+    <slot />
+  </header>
+</template>
