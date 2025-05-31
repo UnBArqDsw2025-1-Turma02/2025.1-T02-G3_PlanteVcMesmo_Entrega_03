0066d8b37e1a72cc54b3e21cec5bf7f09b619214
mateusvrs <mateusvrs.dev@gmail.com>
2025-05-30 23:17:43 -0300

    refactor: uses shadcn input

--- frontend/src/components/base/Input/InputText.vue
+++ frontend/src/components/base/Input/InputText.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { Input } from '@/components/ui/input';
 
 defineProps<{
   modelValue: string;
@@ -13,12 +13,8 @@ defineEmits(['update:modelValue']);
 <template>
   <div class="mb-4">
     <label class="block mb-1">{{ label }}</label>
-    <input
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      type="text"
-      :placeholder="placeholder"
-      class="w-full p-2 border-1 border-[#CEC38F] rounded-lg text-white"
-    />
+    <Input :value="modelValue" @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      type="text" :placeholder="placeholder"
+      class="w-full p-2 border-1 border-[#CEC38F] text-sm rounded-lg text-white placeholder:text-gray-200 focus-visible:ring-0 focus-visible:border-[#CEC38F]" />
   </div>
 </template>
