deafdcc3203c6b7978dcd73bbd3492b94379c12b
Rafael Melo Matuda <134009750+rmatuda@users.noreply.github.com>
2025-05-31 16:53:19 -0300

    Backend/caso post/create-post-usecase (#31)
    * create-post-usecase
    
    * delete-post-usecase

--- backend/src/application/usecases/posts/create-post-usecase.ts
+++ backend/src/application/usecases/posts/create-post-usecase.ts
@@ -1,16 +1,36 @@
 import { Post } from '@/domain';
+import { PostRepository } from '../../../domain/postRepository';
+import { Validator } from '../../services/validator.interface';
+
+export namespace CreatePostUsecase {
+  export type Input = {
+    title: string;
+    content: string;
+    authorId: string;
+  };
+
+  export type Output = Post;
+}
 
 export class CreatePostUsecase {
-  constructor() {}
+  constructor(
+    private readonly postInputValidator: Validator<CreatePostUsecase.Input>,
+    private readonly postRepository: PostRepository,
+  ) {}
 
   public async execute(
     input: CreatePostUsecase.Input,
   ): Promise<CreatePostUsecase.Output> {
-    throw new Error('Method not implemented.');
-  }
-}
+    await this.postInputValidator.validate(input);
 
-export namespace CreatePostUsecase {
-  export type Input = Omit<Post, 'id'>;
-  export type Output = Post;
+    const postDataToCreate: PostRepository.Create.Input = {
+      title: input.title,
+      content: input.content,
+      authorId: input.authorId,
+    };
+
+    const newPost = await this.postRepository.create(postDataToCreate);
+
+    return newPost;
+  }
 }
--- backend/src/application/usecases/posts/delete-post-usecase.ts
+++ backend/src/application/usecases/posts/delete-post-usecase.ts
@@ -1,18 +1,41 @@
 import { Post } from '@/domain';
+import { PostRepository } from '../../../domain/postRepository';
+import { Validator } from '../../services/validator.interface';
+
+export namespace DeletePostUsecase {
+  export type Input = {
+    id: string;
+  };
+
+  export type Output = {
+    success: boolean;
+    message?: string;
+  };
+}
 
 export class DeletePostUsecase {
-  constructor() {}
+  constructor(
+    private readonly deletePostInputValidator: Validator<DeletePostUsecase.Input>,
+    private readonly postRepository: PostRepository,
+  ) {}
 
   public async execute(
     input: DeletePostUsecase.Input,
   ): Promise<DeletePostUsecase.Output> {
-    throw new Error('Method not implemented.');
-  }
-}
+    await this.deletePostInputValidator.validate(input);
 
-export namespace DeletePostUsecase {
-  export type Input = {
-    postId: string;
-  };
-  export type Output = Promise<void>;
+    const wasDeleted = await this.postRepository.delete({ id: input.id });
+
+    if (!wasDeleted) {
+      return {
+        success: false,
+        message: 'Post não encontrado ou não pôde ser deletado.',
+      };
+    }
+
+    return {
+      success: true,
+      message: 'Post deletado com sucesso.',
+    };
+  }
 }
