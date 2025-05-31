0d7eff5a4e1848fe4cef9648e22c9cf9121a72f6
Caio <caiofeliperro@gmail.com>
2025-05-31 16:59:28 -0300

    fix: move label repository path

---
+++ backend/src/application/repositories/label-repository.ts
@@ -0,0 +1,44 @@
+import { Label } from './label';
+
+export namespace LabelRepository {
+  export const name = 'LabelRepository';
+
+  export namespace List {
+    export type Input = {
+      pagination?: {
+        page: number;
+        limit: number;
+      };
+    };
+    export type Output = {
+      labels: Label[];
+      total: number;
+    };
+  }
+
+  export namespace ListMany {
+    export type Input = {
+      filter?: Partial<{
+        ids: string[];
+        names: string[];
+        isActive: boolean;
+      }>;
+      pagination?: {
+        page: number;
+        limit: number;
+      };
+    };
+
+    export type Output = {
+      labels: Label[];
+      total: number;
+    };
+  }
+}
+
+export interface LabelRepository {
+  list(input: LabelRepository.List.Input): Promise<LabelRepository.List.Output>;
+  listMany(
+    input: LabelRepository.ListMany.Input,
+  ): Promise<LabelRepository.ListMany.Output>;
+}
