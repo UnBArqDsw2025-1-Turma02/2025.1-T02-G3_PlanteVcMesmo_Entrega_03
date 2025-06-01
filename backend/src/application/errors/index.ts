08d4a189b25fd75b5299c9da375a93f3c7213b73
Arthur Ribeiro <artrsousa1@gmail.com>
2025-06-01 11:45:15 -0300

    feat: add login with google usecase

--- backend/src/application/errors/index.ts
+++ backend/src/application/errors/index.ts
@@ -1,3 +1,4 @@
 export * from './api-error';
 export * from './llm-provider-error';
 export * from './not-found-error';
+export * from './validation-error';
--- backend/src/infra/container/application/services.ts
+++ backend/src/infra/container/application/services.ts
@@ -1,13 +1,21 @@
 import { InfraDI } from '../infra';
 import { ManualSchedulerStrategy } from '@/infra/services/scheduler/manual-scheduler';
 import { AutomaticSchedulerStrategy } from '@/infra/services/scheduler/automatic-scheduler';
+import {
+  EncryptionCryptoService,
+  GoogleAuthService,
+} from '@/infra/application/services';
+import { JwtServiceImpl } from '@/infra/services';
 
 export function configureServices(container: InfraDI) {
   return container
     .add('ManualSchedulerStrategy', () => new ManualSchedulerStrategy())
     .add('AutomaticSchedulerStrategy', ({ GeminiLLMProvider }) => {
       return new AutomaticSchedulerStrategy(GeminiLLMProvider);
-    });
+    })
+    .add('EncryptionService', () => new EncryptionCryptoService())
+    .add('GoogleAuthService', () => new GoogleAuthService())
+    .add('JwtService', () => new JwtServiceImpl());
 }
 
 export type ServicesDI = ReturnType<typeof configureServices>;
