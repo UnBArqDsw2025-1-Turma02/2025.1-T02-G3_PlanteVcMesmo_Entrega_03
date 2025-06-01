76ba2e61f1d1e212b76e700cf171adc0c24ece09
Rafael Melo Matuda <rafael.matuda10@gmail.com>
2025-06-02 00:58:29 +0900

    add update-post-usecase

--- backend/src/application/usecases/posts/update-post-usecase.ts
+++ backend/src/application/usecases/posts/update-post-usecase.ts
@@ -1,16 +1,29 @@
-import { Post } from '@/domain';
+import { PostRepository } from '@/application/repositories';
+import { Validator } from '@/application/services';
+export namespace UpdatePostUsecase {
+  export type Input = {
+    id: string;
+    fields: PostRepository.Update.Input;
+  };
+  export type Output = PostRepository.Update.Output;
+}
 
 export class UpdatePostUsecase {
-  constructor() {}
+  constructor(
+    private readonly updatePostInputValidator: Validator<UpdatePostUsecase.Input>,
+    private readonly postRepository: PostRepository,
+  ) {}
 
   public async execute(
     input: UpdatePostUsecase.Input,
   ): Promise<UpdatePostUsecase.Output> {
-    throw new Error('Method not implemented.');
-  }
-}
+    const validatedInput = await this.updatePostInputValidator.validate(input);
 
-export namespace UpdatePostUsecase {
-  export type Input = Omit<Post, 'id' | 'createdAt' | 'updatedAt'>;
-  export type Output = Post;
+
+    const updatedPost = await this.postRepository.update(
+      validatedInput.id,
+      validatedInput.fields,
+    );
+    return updatedPost;
+  }
 }
