6294b19fc497ec759a581374f2c628795a9489ed
Caio Felipe <caiofeliperro@gmail.com>
2025-06-01 17:36:53 -0300

    feat: add container usecases (#39)

--- backend/src/infra/container/infra/services.ts
+++ backend/src/infra/container/infra/services.ts
@@ -1,10 +1,12 @@
 import { RepositoriesDI } from './repositories';
 import { ChatGPTProvider, GeminiProvider } from '@/infra/services/chat';
+import { LLMFactory } from '@/infra/factory';
 
 export function configureServices(container: RepositoriesDI) {
   return container
     .add('ChatGPTLLMProvider', () => new ChatGPTProvider())
-    .add('GeminiLLMProvider', () => new GeminiProvider());
+    .add('GeminiLLMProvider', () => new GeminiProvider())
+    .add('LLMFactory', () => new LLMFactory());
 }
 
 export type ServicesDI = ReturnType<typeof configureServices>;
