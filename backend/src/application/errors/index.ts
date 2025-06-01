30cdc07ece62a87ede2440bb0e8fb625add25839
Arthur Ribeiro <artrsousa1@gmail.com>
2025-06-01 19:51:34 -0300

    feat: add authorization middleware

--- backend/src/application/errors/index.ts
+++ backend/src/application/errors/index.ts
@@ -3,3 +3,4 @@ export * from './llm-provider-error';
 export * from './not-found-error';
 export * from './validation-error';
 export * from './unauthorized-error';
+export * from './forbidden-error';
