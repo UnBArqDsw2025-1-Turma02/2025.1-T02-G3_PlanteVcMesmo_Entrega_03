2a9012d13c6c56749837ee263e6149c99b8dbae1
Rafael Melo Matuda <rafael.matuda10@gmail.com>
2025-06-02 00:50:30 +0900

    find-post-usecase

--- backend/src/application/usecases/posts/find-post-usecase.ts
+++ backend/src/application/usecases/posts/find-post-usecase.ts
@@ -1,18 +1,27 @@
 import { Post } from '@/domain';
-
-export class FindPostUsecase {
-  constructor() {}
-
-  public async execute(
-    input: FindPostUsecase.Input,
-  ): Promise<FindPostUsecase.Output> {
-    throw new Error('Method not implemented.');
-  }
-}
+import { PostRepository } from '@/application/repositories';
+import { Validator } from '@/application/services';
 
 export namespace FindPostUsecase {
   export type Input = {
     postId: string;
   };
   export type Output = Post | null;
+}
+
+export class FindPostUsecase {
+  constructor(
+    private readonly findPostInputValidator: Validator<FindPostUsecase.Input>,
+    private readonly postRepository: PostRepository,
+  ) {}
+
+  public async execute(
+    input: FindPostUsecase.Input,
+  ): Promise<FindPostUsecase.Output> {
+    const validatedInput = await this.findPostInputValidator.validate(input);
+    const post = await this.postRepository.findBy({
+      id: validatedInput.postId,
+    });
+    return post;
+  }
 }
